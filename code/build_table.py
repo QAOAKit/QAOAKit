766a2af59f67dafcc14851195ae57ecbfbd2f0b5
Ruslan Shaydulin <rshaydu@g.clemson.edu>
2021-04-20 16:57:36 -0400

    added mapping from pynauty cert to graph_id

--- code/build_table.py
+++ code/build_table.py
@@ -1,7 +1,23 @@
 import networkx as nx
 import re
 import copy
+import pynauty
+import pickle
 
+def get_adjacency_dict(G):
+    """Returns adjacency dictionary for G
+    G must be a networkx graph
+    Return format: { n : [n1,n2,...], ... }
+    where [n1,n2,...] is a list of neighbors of n
+    """
+    adjacency_dict = {}
+    for n, neigh_dict in G.adjacency():
+        neigh_list = []
+        for neigh, attr_dict in neigh_dict.items():
+            assert(len(attr_dict) == 0)
+            neigh_list.append(neigh)
+        adjacency_dict[n] = neigh_list
+    return adjacency_dict
 
 n_qubits=7
 p=3
@@ -20,7 +36,10 @@ bipartite_count=0
 planar_count=0
 eulerian_count=0
 
-graphs = {}
+graph_id2pynautycert = {}
+graph_id2graph = {}
+pynautycert2graph_id = {}
+pynautycert2graph = {}
 
 with open("../data/qaoa-dataset-version1/Graphs/graph"+str(n_qubits)+"c.txt") as f:
     for graph in range(1,n_graphs[n_qubits]+1):
@@ -40,6 +59,17 @@ with open("../data/qaoa-dataset-version1/Graphs/graph"+str(n_qubits)+"c.txt") as
                 if adj_str[m]=='1':
                     edges.append([n,q_num])
                     G.add_edge(n,q_num)
-        graphs[graph_id] = copy.deepcopy(G) 
+        g = pynauty.Graph(number_of_vertices=G.number_of_nodes(), directed=nx.is_directed(G),
+                    adjacency_dict = get_adjacency_dict(G))
+        cert = pynauty.certificate(g)
+        graph_id2graph[graph_id] = copy.deepcopy(G) 
+        graph_id2pynautycert[graph_id] = cert
+        pynautycert2graph_id[cert] = graph_id
+        pynautycert2graph[cert] = copy.deepcopy(G)
 
-print(graphs)
+assert(len(graph_id2pynautycert) == n_graphs[n_qubits])
+assert(len(graph_id2graph) == n_graphs[n_qubits])
+assert(len(pynautycert2graph_id) == n_graphs[n_qubits])
+assert(len(pynautycert2graph) == n_graphs[n_qubits])
+
+pickle.dump((graph_id2pynautycert,graph_id2graph,pynautycert2graph_id,pynautycert2graph), open(f"../data/lookup_tables/graph2pynauty_tables_n{n_qubits}.p", "wb"))
