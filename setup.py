c7c7dd13f74db2342788f4173a7dc8dd0d54d5aa
Kunal Marwaha <marwahaha@berkeley.edu>
2021-06-09 10:35:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(name='QAOA_parameters',
 	author='Ruslan Shaydulin',
 	author_email='rshaydu@g.clemson.edu',
 	packages=['QAOA_parameters'],
-    install_requires=['qiskit==0.25.2','pandas','networkx','numpy','pytest'],
+    install_requires=['qiskit==0.25.2','pynauty==1.0.0', 'pandas','networkx','numpy','pytest'],
 	zip_safe=False)
