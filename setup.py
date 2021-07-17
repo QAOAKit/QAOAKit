b169ad655f298dd5df3496b2facead3ca28f89f0
Kunal Marwaha <marwahaha@berkeley.edu>
2021-07-17 08:31:37 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
-setup(name='QAOA_parameters',
-	description='Tools for using the pre-optimized parameters from ORNL dataset (and more)',
-	author='Ruslan Shaydulin',
-	author_email='rshaydu@g.clemson.edu',
-	packages=['QAOA_parameters'],
-    install_requires=['qiskit==0.25.2','pynauty==1.0.0', 'pandas','networkx','numpy','pytest'],
-	zip_safe=False)
+setup(
+    name='QAOA_parameters',
+    version='0.1.0',
+    description='Tools for using the pre-optimized parameters from ORNL dataset (and more)',
+    author='Ruslan Shaydulin',
+    author_email='rshaydu@g.clemson.edu',
+    python_requires='>=3, <4',
+    packages=['QAOA_parameters'],
+    install_requires=['qiskit==0.25.2', 'pynauty==1.0.0',
+                      'pandas', 'networkx', 'numpy', 'pytest'],
+    zip_safe=True
+)
