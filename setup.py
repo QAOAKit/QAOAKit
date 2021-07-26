66ff7a68f9d166bd1c58e864c9843fdd85b8dc51
Kunal Marwaha <marwahaha@berkeley.edu>
2021-07-25 23:23:07 -0700

    Bump qiskit version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='rshaydu@g.clemson.edu',
     python_requires='>=3, <4',
     packages=['QAOA_parameters'],
-    install_requires=['qiskit==0.25.2', 'pynauty==1.0.0',
+    install_requires=['qiskit>=0.28.0', 'pynauty==1.0.0',
                       'pandas', 'networkx', 'numpy', 'pytest'],
     zip_safe=True
 )
